9027b6557ec2c1533beafe3a288ac30015251910
andriani <a.christanty@gmail.com>
2022-04-15 13:44:06 +0800

    need to work on tooltip still

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,223 +1,148 @@
-//create the dimension for our svg
+// The code for the chart is wrapped inside a function that
+// automatically resizes the chart
+function makeResponsive() {
 
-var svgWidth = 960;
-var svgHeight = 500;
+  // if the SVG area isn't empty when the browser loads,
+  // remove it and replace it with a resized version of the chart
+  var svgArea = d3.select("body").select("svg");
 
-var margin = {
-    top: 20,
-    right: 40,
-    bottom: 80,
-    left: 100
+  // clear svg is not empty
+  if (!svgArea.empty()) {
+    svgArea.remove();
+  }
+
+  // SVG wrapper dimensions are determined by the current width and
+  // height of the browser window.
+  var svgWidth = window.innerWidth;
+  var svgHeight = window.innerHeight;
+
+  var margin = {
+    top: 50,
+    bottom: 50,
+    right: 50,
+    left: 50
   };
 
-  var width = svgWidth - margin.left - margin.right;
   var height = svgHeight - margin.top - margin.bottom;
+  var width = svgWidth - margin.left - margin.right;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-var svg = d3
-  .select(".chart")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+  // Append SVG element
+  var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
 
-  // Append an SVG group
-var chartGroup = svg.append("g")
-.attr("transform", `translate(${margin.left}, ${margin.top})`);
+  // Append group element
+  var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Initial Params////?????????????????????????????????????????????????????????????????????/
-var chosenXAxis = "obesity";
+  // Read CSV
+  d3.csv("data.csv").then(function(hData) {
 
-function xScale(hData, chosenXAxis) {
+    // create date parser
+  //  var dateParser = d3.timeParse("%d-%b");
+
+    // parse data
+    hData.forEach(function(data) {
+//      data.date = dateParser(data.date);
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare;
+      data.abbr = data.abbr;
+    });
+
     // create scales
     var xLinearScale = d3.scaleLinear()
-      .domain([d3.min(hData, d => d[chosenXAxis]) * 0.8,
-        d3.max(hData, d => d[chosenXAxis]) * 1.2
-      ])
+      .domain(d3.extent(hData, d => d.poverty))
       .range([0, width]);
-  
-    return xLinearScale;
-  
-  }
 
-// function used for updating xAxis var upon click on axis label
-function renderAxes(newXScale, xAxis) {
-    var bottomAxis = d3.axisBottom(newXScale);
-  
-    xAxis.transition()
-      .duration(1000)
-      .call(bottomAxis);
-  
-    return xAxis;
-  }
-
-
-// function used for updating circles group with a transition to
-// new circles
-function renderCircles(circlesGroup, newXScale, chosenXAxis) {
-
-    circlesGroup.transition()
-      .duration(1000)
-      .attr("cx", d => newXScale(d[chosenXAxis]));
-  
-    return circlesGroup;
-  }
-
- // function used for updating circles group with new tooltip
-function updateToolTip(chosenXAxis, circlesGroup) {
-
-    var label;
-  
-    if (chosenXAxis === "obesity") {
-      label = "Obesity";
-    }
-    else {
-      label = "Income";
-    }
-    var toolTip = d3.tip()
-    .attr("class", "tooltip")
-    ///////////////////////////////////////////////////////////////????????????????????????????????/////////////
-    .offset([80, -60])
-    .html(function(d) {
-      return (`${d.state}<br>${label} ${d[chosenXAxis]}`);
-    });
-
-  circlesGroup.call(toolTip);
-
-  circlesGroup.on("mouseover", function(data) {
-    toolTip.show(data);
-  })
-    // onmouseout event
-    .on("mouseout", function(data, index) {
-      toolTip.hide(data);
-    });
-
-  return circlesGroup;
-}
-
-
-// Retrieve data from the CSV file and execute everything below
-d3.csv("data.csv").then(function(healthData, err) {
-    console.log(healthData)
-    if (err) throw err;
-    console.log(healthData);
-    // parse data
-    healthData.forEach(function(data) {
-      data.obesity = +data.obesity;
-      data.income = +data.income;
-      data.poverty = +data.poverty;
-      data.healthcare = +data.healthcare
-    });
-  
-    // xLinearScale function above csv import
-    var xLinearScale = xScale(healthData, chosenXAxis);
-  
-    // Create y scale function
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(healthData, d => d.poverty)])
+      .domain([0, d3.max(hData, d => d.healthcare)])
       .range([height, 0]);
-  
-    // Create initial axis functions
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
-  
-    // append x axis
-    var xAxis = chartGroup.append("g")
-      .classed("x-axis", true)
-      .attr("transform", `translate(0, ${height})`)
-      .call(bottomAxis);
-  
-    // append y axis
+
+    // create axes
+    var xAxis = d3.axisBottom(xLinearScale);
+    var yAxis = d3.axisLeft(yLinearScale).ticks(20);
+
+    // append axes
     chartGroup.append("g")
-      .call(leftAxis);
-  
-    // append initial circles
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxis);
+
+    chartGroup.append("g")
+      .call(yAxis);
+
+    // line generator
+ //   var line = d3.line()
+ //     .x(d => xLinearScale(d.poverty))
+ //     .y(d => yLinearScale(d.healthcare));
+
+    // append line
+ //   chartGroup.append("path")
+ //     .data([hData])
+ //     .attr("d", line)
+ //     .attr("fill", "none")
+ //     .attr("stroke", "red");
+
+    // append circles
     var circlesGroup = chartGroup.selectAll("circle")
-      .data(healthData)
+      .data(hData)
       .enter()
       .append("circle")
-      .attr("cx", d => xLinearScale(d[chosenXAxis]))
-      .attr("cy", d => yLinearScale(d.poverty))
-      .attr("r", 20)
-      .attr("fill", "pink")
-      .attr("opacity", ".5");
-  
-    // Create group for two x-axis labels
-    var labelsGroup = chartGroup.append("g")
-      .attr("transform", `translate(${width / 2}, ${height + 20})`);
-  
-    var obesityLabel = labelsGroup.append("text")
-      .attr("x", 0)
-      .attr("y", 20)
-      .attr("value", "obesity") // value to grab for event listener
-      .classed("active", true)
-      .text("Obesity");
-  
-    var axisLabel = labelsGroup.append("text")
-      .attr("x", 0)
-      .attr("y", 40)
-      .attr("value", "income") // value to grab for event listener
-      .classed("inactive", true)
-      .text("Income");
-  
-    // append y axis
-    chartGroup.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x", 0 - (height / 2))
-      .attr("dy", "1em")
-      .classed("axis-text", true)
-      .text("Poverty");
-  
-    // updateToolTip function above csv import
-    var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-  //////////////////////////////////////////////////////////////////////////////////////////////////
+      .attr("cx", d => xLinearScale(d.poverty))
+      .attr("cy", d => yLinearScale(d.healthcare))
+      .attr("r", "10")
+      .attr("fill", "gold")
+      .attr("stroke-width", "1")
+      .attr("stroke", "black");
 
-    // x axis labels event listener
-    labelsGroup.selectAll("text")
-      .on("click", function() {
-        // get value of selection
-        var value = d3.select(this).attr("value");
-        if (value !== chosenXAxis) {
-  
-          // replaces chosenXAxis with value
-          chosenXAxis = value;
-  
-          // console.log(chosenXAxis)
-  
-          // functions here found above csv import
-          // updates x scale for new data
-          xLinearScale = xScale(healthData, chosenXAxis);
-  
-          // updates x axis with transition
-          xAxis = renderAxes(xLinearScale, xAxis);
-  
-          // updates circles with new x values
-          circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
-  
-          // updates tooltips with new info
-          circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-  
-          // changes classes to change bold text
-          if (chosenXAxis === "obesity") {
-            axisLabel
-              .classed("active", true)
-              .classed("inactive", false);
-            hairLengthLabel
-              .classed("active", false)
-              .classed("inactive", true);
-          }
-          else {
-            albumsLabel
-              .classed("active", false)
-              .classed("inactive", true);
-            hairLengthLabel
-              .classed("active", true)
-              .classed("inactive", false);
-          }
-        }
+      console.log(hData);
+
+    // Step 1: Append tooltip div
+   
+    var toolTip = d3
+    .tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .style("color", "black")
+    .style("background", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+    .html(function (d) {
+      return `${d.abbr}`;
+    });
+
+  // Step 7: Create tooltip in the chart
+  // ==============================
+  circlesGroup.call(toolTip);
+  /*
+    var toolTip = d3.select("body")
+      .append("div")
+      .classed("tooltip", true);
+
+    // Step 2: Create "mouseover" event listener to display tooltip
+    circlesGroup.on("mouseover", function(d) {
+      toolTip.style("display", "block")
+          .html(
+            `<strong>${dateFormatter(d.poverty)}<strong><hr>${d.healthcare}
+        medal(s) won`)
+          .style("left", d3.event.pageX + "px")
+          .style("top", d3.event.pageY + "px");
+    })
+      // Step 3: Create "mouseout" event listener to hide tooltip
+      .on("mouseout", function() {
+        toolTip.style("display", "none");
       });
+*/
   }).catch(function(error) {
-   console.log(error);
+    console.log(error);
   });
-  
+}
+
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);
