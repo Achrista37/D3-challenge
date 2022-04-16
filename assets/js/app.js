08067aa632e4ec2809f85a7c16db8cc6b34d5ce0
andriani <a.christanty@gmail.com>
2022-04-17 03:43:30 +0800

    cleaned unused lines of codes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,3 @@
-// The code for the chart is wrapped inside a function that
-// automatically resizes the chart
-/*function makeResponsive() {
-
-  // if the SVG area isn't empty when the browser loads,
-  // remove it and replace it with a resized version of the chart
-  var svgArea = d3.select("body").select("svg");
-
-  // clear svg is not empty
-  if (!svgArea.empty()) {
-    svgArea.remove();
-  }
-*/
 
   // SVG wrapper dimensions are determined by the current width and
   // height of the browser window.
@@ -18,10 +5,10 @@
   var svgHeight = window.innerHeight;
 
   var margin = {
-    top: 50,
-    bottom: 50,
-    right: 50,
-    left: 50
+    top: 80,
+    bottom: 80,
+    right: 80,
+    left: 80
   };
 
   var height = svgHeight - margin.top - margin.bottom;
@@ -34,10 +21,6 @@
     .attr("height", svgHeight)
     .attr("width", svgWidth);
     
-    
-
-  // Append group element
-  //var formatAxis = d3.format("  0");
 
 
   //var axisText = d3.select("#scatter").append("text").attr("class","x-label").attr("text-anchor","end").attr("x",15).attr("y",0).text("------------------------------------------------")
@@ -47,12 +30,8 @@
   // Read CSV
   d3.csv("data.csv").then(function(hData) {
 
-    // create date parser
-  //  var dateParser = d3.timeParse("%d-%b");
-
     // parse data
     hData.forEach(function(data) {
-//      data.date = dateParser(data.date);
       data.poverty = +data.poverty;
       data.healthcare = +data.healthcare;
       data.abbr = data.abbr;
@@ -94,7 +73,7 @@
       .attr("stroke-width", "1")
       .attr("stroke", "black")
       .attr('opacity', 0.2)
-     // .html(function (d) {return `${d.abbr}`})
+
       ;
       chartGroup
       .append("text")
@@ -117,92 +96,23 @@
       .data(hData, function (d) {
         console.log(d);
         d3.select(this).append("text")
-       // .select("text")
+ 
         .text(d => d.abbr)
         .attr("text-anchor", "middle")
         .attr("font-size", "12px")
         .attr("fill", "black")
         .attr("x", (d) => xLinearScale(d.poverty))
         .attr("y", (d) => yLinearScale(d.healthcare))
-     //   .attr("dy", -395)
+
       })
-     // .enter()
-     /* .each(function (circle) {
-        console.log(this);
-        d3.select(this).append("text")
-        .select("text")
-        .text((d) => d.abbr)
-        .attr("text-anchor", "middle")
-        .attr("font-size", "12px")
-        .attr("fill", "black")
-        })
-      */
-    /*
-      .attr("x", (d) => xLinearScale(d.poverty))
-      .attr("y", (d) => yLinearScale(d.healthcare))
-      .attr("dy", -395)
-      */
+     
       ;
 
       console.log(hData);
       
   
- 
-    // Step 1: Append tooltip div
-   /*
-    var toolTip = d3
-    .tooltip()
-    .attr("class", "tooltip")
-    .offset([0, 0])
-    .style("color", "black")
-    .style("background", "white")
-    .style("border", "solid")
-    .style("border-width", "1px")
-    .style("border-radius", "5px")
-    .style("padding", "5px")
-    .html(function (d) {
-      return `${d.abbr}`;
-    });
-*/
-    circlesGroup.call(toolTip);
-/*
-    svg.selectAll("text")
-      .data(hData)
-      .enter()
-      .append("text")
-      .attr('x', (item) => {return item[0]})
-  
-*/
 
-  // Step 7: Create tooltip in the chart
-  // ==============================
-  //circlesGroup.call(toolTip);
-  /*
-    var toolTip = d3.select("body")
-      .append("div")
-      .classed("tooltip", true);
 
-    // Step 2: Create "mouseover" event listener to display tooltip
-    circlesGroup.on("mouseover", function(d) {
-      toolTip.style("display", "block")
-          .html(
-            `<strong>${dateFormatter(d.poverty)}<strong><hr>${d.healthcare}
-        medal(s) won`)
-          .style("left", d3.event.pageX + "px")
-          .style("top", d3.event.pageY + "px");
-    })
-      // Step 3: Create "mouseout" event listener to hide tooltip
-      .on("mouseout", function() {
-        toolTip.style("display", "none");
-      });
-*/
   }).catch(function(error) {
     console.log(error);
   });
-
-
-// When the browser loads, makeResponsive() is called.
-//makeResponsive();
-
-// When the browser window is resized, makeResponsive() is called.
-d3.select(window).on("resize", makeResponsive);
